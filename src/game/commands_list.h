2a712d8e4a7d5a379e6899552e42663d1776e5f6
CGLemon <cglemon000@gmail.com>
2021-11-02 01:20:11 +0800

    Remove kgs-chat, because it may crash on kgsGtp.

--- src/game/commands_list.h
+++ src/game/commands_list.h
@@ -2,7 +2,7 @@
 
 static constexpr auto kProtocolVerion = 2;
 
-static const std::array<std::string, 26> kGtpCommandsList = {
+static const std::array<std::string, 25> kGtpCommandsList = {
     // Part of GTP version 2 standard command
     "protocol_version",
 
@@ -72,9 +72,7 @@ static const std::array<std::string, 26> kGtpCommandsList = {
 
     "genmove_analyze",
 
-    "kgs-game_over",
-
-    "kgs-chat"
+    "kgs-game_over"
 };
 
 
