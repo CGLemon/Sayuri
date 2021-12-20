1ded928b4d59315147978312ffd915c0aa8739e8
CGLemon <cglemon000@gmail.com>
2021-12-20 14:05:04 +0800

    Select the reasonable coefficients for NormToTApprox function.

--- src/mcts/lcb.h
+++ src/mcts/lcb.h
@@ -9,8 +9,8 @@
 static double RationalApprox(double t) {
     // Abramowitz and Stegun formula 26.2.23.
     // The absolute value of the error should be less than 4.5 e-4.
-    constexpr double c[] = {2.515517, 0.802853, 0.010328};
-    constexpr double d[] = {1.432788, 0.189269, 0.001308};
+    constexpr double c[3] = {2.515517, 0.802853, 0.010328};
+    constexpr double d[3] = {1.432788, 0.189269, 0.001308};
     return t - ((c[2]*t + c[1])*t + c[0]) / 
                    (((d[2]*t + d[1])*t + d[0])*t + 1.0);
 }
@@ -36,11 +36,11 @@ static double NormalCdfInverse(double p) {
 
 // From the: https://github.com/lightvector/KataGo/blob/master/cpp/core/fancymath.h
 static double NormToTApprox(double z, double n) {
-    constexpr double a = 0.85;
-    constexpr double b = 1;
-    constexpr double c = 1;
+    constexpr double a = 0.853999327911;
+    constexpr double b = 1.044042304114;
+    constexpr double c = 0.954115472059;
 
-    return std::sqrt(n * exp(z * z * (n-a) / ((n-b) * (n-c))) - n);
+    return std::sqrt(n * std::exp(z * z * (n-a) / ((n-b) * (n-c))) - n);
 }
 
 class LcbEntries {
@@ -56,7 +56,7 @@ public:
 private:
     static constexpr int kEntrySize = 1000;
 
-    std::array<float, kEntrySize> _z_lookup_table;
+    std::array<float, kEntrySize> z_lookup_table_;
 };
 
 inline LcbEntries& LcbEntries::Get() {
@@ -68,19 +68,19 @@ inline void LcbEntries::Initialize(float complement_probability) {
     const double z = NormalCdfInverse(1.f - complement_probability);
 
     for (int i = 0; i < kEntrySize; ++i) {
-        _z_lookup_table[i] = NormToTApprox(z, i+2);
+        z_lookup_table_[i] = NormToTApprox(z, i+2);
     }
 }
 
 inline float LcbEntries::CachedTQuantile(int v) {
     if (v < 1) {
-        return _z_lookup_table[0];
+        return z_lookup_table_[0];
     }
     if (v < kEntrySize) {
-        return _z_lookup_table[v - 1];
+        return z_lookup_table_[v - 1];
     }
     // z approaches constant when v is high enough.
     // With default lookup table size the function is flat enough that we
     // can just return the last entry for all v bigger than it.
-    return _z_lookup_table[kEntrySize - 1];
+    return z_lookup_table_[kEntrySize - 1];
 }
