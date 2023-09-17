f0eab38cf522935031b583296b1a3fa2051ff419
CGLemon <cglemon000@gmail.com>
2023-09-18 01:16:57 +0800

    Clean the book.

--- src/game/book.h
+++ src/game/book.h
@@ -17,23 +17,28 @@ public:
     // Load the opening book.
     void LoadBook(std::string book_name);
 
-    // Try to find the opening moves. Return kPass if there is
+    // Try to find the opening moves. Return false if there is
     // no move in the book.
     bool Probe(const GameState &state, int &book_move) const;
 
     std::vector<std::pair<float, int>> GetCandidateMoves(const GameState &state) const;
 
+    std::string GetVerbose() const;
+
 private:
     using VertexFrequencyList = std::vector<std::pair<int ,int>>;
     using VertexProbabilityList = std::vector<std::pair<int ,float>>;
 
-    std::unordered_map<std::uint64_t, VertexProbabilityList> data_;
+    template <typename T>
+    using BookMap = std::unordered_map<std::uint64_t, T>;
+
+    BookMap<VertexProbabilityList> data_;
 
     void BookDataProcess(std::string sgfstring,
-                         std::unordered_map<std::uint64_t, VertexFrequencyList> &book_data) const;
-
+                         Book::BookMap<VertexFrequencyList> &book_data) const;
 
     static constexpr int kBookBoardSize = 19;
     static constexpr int kMaxBookMoves = 30;
     static constexpr int kFilterThreshold = 25;
+    static constexpr int kMaxSgfGames = 100000;
 };
