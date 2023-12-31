8342d9f449f1654ae988151c7f1eea89b8e2ceee
CGLemon <cglemon000@gmail.com>
2024-01-01 02:59:47 +0800

    Add missed file.

---
+++ train/torch/main.py
@@ -0,0 +1,19 @@
+import argparse
+
+from train import TrainingPipe
+from config import gather_config
+
+def main(cfg):
+    pipe = TrainingPipe(cfg)
+    pipe.fit_and_store()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-j", "--json", metavar="<string>",
+                        help="The setting json file name.", type=str)
+    args = parser.parse_args()
+
+    if args.json == None:
+        print("Please give the setting json file.")
+    else:
+        main(gather_config(args.json))
